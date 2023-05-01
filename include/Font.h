ed8161cd912feb34530ec07a6443427bceac9b18
kewldan <40865857+kewldan@users.noreply.github.com>
2023-05-02 01:29:01 +0300

    Font init

--- include/Font.h
+++ include/Font.h
@@ -1,7 +1,18 @@
 #pragma once
 
+#include "glm/ext.hpp"
+#include "File.h"
+#include "Texture.h"
+#include <string>
+#include <sstream>
+
 namespace Engine {
     class Font {
-
+    private:
+        Texture* texture;
+    public:
+        explicit Font(const char* name, bool from_file = false);
+        char* format(const char* fmt, ...);
+        void text(float x, float y, const char* text, glm::vec3 color = glm::vec3(0));
     };
 }
--- src/Font.cpp
+++ src/Font.cpp
@@ -1,1 +1,32 @@
 #include "Font.h"
+
+Engine::Font::Font(const char *name, bool from_file) {
+    static char *path = new char[128];
+    strcpy_s(path, 128, name);
+    strcat_s(path, 128, ".png");
+
+    texture = new Texture(path, from_file);
+    path[strlen(path) - 4] = 0;
+    strcat_s(path, 128, ".fnt");
+    const char* font = Engine::File::readString(path);
+
+    const char* info_str = strstr(font, "info ");
+    int size = strtol(strstr(info_str, "size=") + 5, nullptr, 10);
+    int stretchH = strtol(strstr(info_str, "stretchH=") + 9, nullptr, 10);
+    int smooth = strtol(strstr(info_str, "smooth=") + 7, nullptr, 10);
+    int aa = strtol(strstr(info_str, "aa=") + 3, nullptr, 10);
+    int paddingL = strtol(strstr(info_str, "padding=") + 8, nullptr, 10);
+    int paddingR = strtol(strstr(info_str, ",") + 1, nullptr, 10);
+    int paddingTop = strtol(strstr(info_str, ",") + 1, nullptr, 10);
+    int paddingBottom = strtol(strstr(info_str, ",") + 1, nullptr, 10);
+    int spacingX = strtol(strstr(info_str, "spacing=") + 8, nullptr, 10);
+    int spacingY = strtol(strstr(info_str, ",") + 1, nullptr, 10);
+}
+
+char *Engine::Font::format(const char *fmt, ...) {
+    return nullptr;
+}
+
+void Engine::Font::text(float x, float y, const char *text, glm::vec3 color) {
+    //
+}
